9d608d9485ee0fbfe8eedf851616da92d5330ab8
reddyaravind <45725968+reddyaravind@users.noreply.github.com>
2024-09-12 14:54:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,10 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired AWS region
 }
 
-# Data source to fetch IAM user details
-data "aws_iam_user" "example" {
-  user_name = "aravind"  # Replace with your IAM user name
-}
+# Data source to fetch AWS account details
+data "aws_caller_identity" "current" {}
 
-# Output the user details
-output "user_arn" {
-  value = data.aws_iam_user.example.arn
-}
-
-output "user_path" {
-  value = data.aws_iam_user.example.path
-}
-
-output "user_unique_id" {
-  value = data.aws_iam_user.example.unique_id
+# Output the AWS account ID
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
