5f1ce3b7d510637371eb56f7a417d3d8cee50578
reddyaravind <45725968+reddyaravind@users.noreply.github.com>
2024-09-12 14:51:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,20 @@ provider "aws" {
   region = "us-west-2"  # Change this to your desired AWS region
 }
 
-# Define an S3 bucket
-resource "aws_s3_bucket" "example_bucket" {
-  bucket_prefix = "my-terraform-bucket-"  # Bucket name prefix
+# Data source to fetch IAM user details
+data "aws_iam_user" "example" {
+  user_name = "aravind"  # Replace with your IAM user name
 }
 
-# Output the name of the created bucket
-output "bucket_name" {
-  value = aws_s3_bucket.example_bucket.bucket
+# Output the user details
+output "user_arn" {
+  value = data.aws_iam_user.example.arn
+}
+
+output "user_path" {
+  value = data.aws_iam_user.example.path
+}
+
+output "user_unique_id" {
+  value = data.aws_iam_user.example.unique_id
 }
