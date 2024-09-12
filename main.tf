afcf601509ceb4d540a72f4df7d0a67827815413
reddyaravind <45725968+reddyaravind@users.noreply.github.com>
2024-09-12 14:37:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,12 @@
+# Define the AWS provider
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_iam_role" "jenkins_role" {
-  name = "jenkins_role"
+# Data source to fetch all IAM users
+data "aws_iam_users" "all_users" {}
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "jenkins_policy" {
-  role       = aws_iam_role.jenkins_role.name
-  policy_arn  = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+# Output the list of IAM user names
+output "iam_user_names" {
+  value = data.aws_iam_users.all_users.users[*].name
 }
